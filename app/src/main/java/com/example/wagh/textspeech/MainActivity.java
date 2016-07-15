b1c1d60b565d143e0ce91b7ee829d7005974d8b2
studymadara <virajwagh6@gmail.com>
2016-07-15 22:54:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/wagh/textspeech/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.wagh.textspeech;
+
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
+
+    EditText et1;
+    TextToSpeech tts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button bb=(Button)findViewById(R.id.listen);
+        et1=(EditText)findViewById(R.id.gettext);
+        tts = new TextToSpeech(this,this);
+
+
+        bb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                speakout();
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onInit(int i) {
+
+    }
+
+    public void speakout()
+    {
+     String s=et1.getText().toString();
+     tts.speak(s,TextToSpeech.QUEUE_FLUSH,null);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        tts.setPitch(0.1f);
+        tts.speak("Hello there",TextToSpeech.QUEUE_FLUSH,null);
+    }
+}
